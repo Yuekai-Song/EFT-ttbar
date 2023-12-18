a7acb8f9dfbb54c2a83807b016cfe540f7afe869
Yuekai-Song <yuehkai.sung@gmail.com>
2023-12-18 14:00:47 +0800

    changed the bins of likelihood

--- select_analysis/prepare/prepare_datacard_3D.cpp
+++ select_analysis/prepare/prepare_datacard_3D.cpp
@@ -3,11 +3,10 @@ void prepare_datacard_3D(int i, int year, int option){
     TString cuts[] = {"(jet_num == 3 && (!lep_flavour))","(jet_num >= 4  && (!lep_flavour))",
                       "(jet_num == 3 && lep_flavour)",  "(jet_num >= 4 && lep_flavour)"};
     TString cutsName[] = {"E_3jets", "E_4jets", "M_3jets", "M_4jets"};
-    int xyz_bins[] = {270, 40, 37};
-    double xyz_range[] = {300, 3000, 0, 4.0, 13, 50};
+    int xyz_bins[] = {270, 40, 40};
+    double xyz_range[] = {300, 3000, 0, 4.0, 5, 45};
 
     cout<<"begin "<<cutsName[i]<<":"<<endl;
     prepare_3D* p = new prepare_3D(cuts[i], cutsName[i], year, xyz_bins, xyz_range, option);
     delete p;
-
 }
